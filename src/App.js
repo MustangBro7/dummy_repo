29307a660b7ec96430f14ad147fba321faa11dc2
Abhinav Mohan <abhinavmohan12@gmail.com>
2023-11-22 22:00:58 +0530

    initial frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import "../src/App.css";  
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import React from "react";
+// import Contentpage from "../src/components/Contentpage";
+// import Login from "../src/components/Login";
+// import SignUp from "../src/components/SignUp";
+// import UserPage from "../src/components/UserPage";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// function App() {
+//   return (
+//     <div className="App">
+
+//       {/* <Contentpage />
+//       <SignUp /> */}
+//       <Router>
+//         <Routes>
+//           <Route exact path= "/" element = {<Contentpage/>}></Route>
+//           <Route exact path="/login" element={<Login />} />
+//           <Route exact path="/SignUp" element={<SignUp />} />
+//         </Routes>
+      
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Contentpage from "./components/Contentpage";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import UserPage from "./components/UserPage";
+import { VehicleForm } from "./components/VehicleForm";
+import AddMoney from "./components/AddMoney";
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route exact path="/source" element={<Contentpage />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/SignUp" element={<SignUp />} />
+          <Route exact path="/user/:id" element={<UserPage />} />
+          <Route exact path="/user/:id/vehicleForm" element={<VehicleForm />} />
+          <Route exact path="/user/:id/AddMoney" element={<AddMoney />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/HeaderNav.js
@@ -0,0 +1,28 @@
+import React from "react";
+export const HeaderNav = () => {
+  return (
+    <div>
+      <myNavbar>
+        <h3 style={{ color: "#494d5f", fontWeight: "900", fontFamily: "Lato" }}>
+          Auto-Toll
+        </h3>
+        <nav>
+          <ul className="nav__links">
+            <li>
+              <a href="#Home">Home</a>
+            </li>
+            <li>
+              <a href="#Service">Services</a>
+            </li>
+            <li>
+              <a href="#About">About</a>
+            </li>
+          </ul>
+        </nav>
+        <a className="cta" href="#button">
+          <button>Contact</button>
+        </a>
+      </myNavbar>
+    </div>
+  );
+};
